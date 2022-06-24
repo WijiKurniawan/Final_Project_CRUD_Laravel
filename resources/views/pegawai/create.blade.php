1ab3c613d1a39710a4224837ee1aaffbf7df727d
WijiKurniawan <wijikurniawan121@gmail.com>
2022-06-24 22:16:20 +0700

    perbaikan index

--- resources/views/pegawai/create.blade.php
+++ resources/views/pegawai/create.blade.php
@@ -3,6 +3,17 @@
 <form action="">
     <input type="text" name="NIP" placeholder="NIP"><br>
     <input type="text" name="Nama" placeholder="Nama"><br>
-    <input type="text" name="Alamat" placeholder="Alamat"><br>
+    <input type="text" name="TempatLahir" placeholder="Tempat Lahir"><br>
+    <input type="date" name="TanggalLahir" placeholder="Tanggal Lahir"><br>
+    <input type="text" name="Agama" placeholder="Agama"><br>
+    <select name="JenisKelamin">
+        <option value="Pilih Jenis Kelamin"></option>
+        <option value="Laki Laki"></option>
+        <option value="Perempuan"></option>
+    </select><br>
+    <input type="text" name="NoHP" placeholder="Nomor HP"><br>
+    <textarea name="Alamat" id="" cols="30" rows="10"></textarea><br>
+    <input type="submit" name="submit" value="Simpan">
+
 
 </form>
--- resources/views/pegawai/index.blade.php
+++ resources/views/pegawai/index.blade.php
@@ -3,90 +3,104 @@
 @section('title','data pegawai')
 
 @section('content')
-<nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Web Kepegawaian</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarScroll">
-      <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Pegawai</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Lainnya
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
-            <li><a class="dropdown-item" href="{{url('absen')}}">Absen</a></li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
-            <li><a class="dropdown-item" href="{{url('lembur')}}">Lembur</a></li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
-            <li><a class="dropdown-item" href="{{url('jabatan')}}">Jabatan</a></li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
-            <li><a class="dropdown-item" href="{{url('riwayatjabatan')}}">Riwayat jabatan</a></li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
-            <li><a class="dropdown-item" href="{{url('pendidikan')}}">Pendidikan</a></li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
-            <li><a class="dropdown-item" href="{{url('pengalamankerja')}}">Pengalaman Kerja</a></li>
+<!DOCTYPE html>
+<html lang="en">
 
-          </ul>
-        </li>
-        <li class="nav-item">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title></title>
+</head>
 
-        </li>
-      </ul>
+<body>
+  <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Web Kepegawaian</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarScroll">
+        <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Pegawai</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Lainnya
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+              <li><a class="dropdown-item" href="{{url('absen')}}">Absen</a></li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li><a class="dropdown-item" href="{{url('lembur')}}">Lembur</a></li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li><a class="dropdown-item" href="{{url('jabatan')}}">Jabatan</a></li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li><a class="dropdown-item" href="{{url('riwayatjabatan')}}">Riwayat jabatan</a></li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li><a class="dropdown-item" href="{{url('pendidikan')}}">Pendidikan</a></li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li><a class="dropdown-item" href="{{url('pengalamankerja')}}">Pengalaman Kerja</a></li>
+
+            </ul>
+          </li>
+          <li class="nav-item">
+
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
-</nav>
+  </nav>
 
-<a href="/pegawai/create"> + Tambah Pegawai Baru</a>
+  <a href="/pegawai/create"> + Tambah Pegawai Baru</a>
 
-<br />
-<br />
+  <br />
+  <br />
 
-<table border="1">
-  <tr>
-    <th>id</th>
-    <th>NIP</th>
-    <th>Nama</th>
-    <th>Alamat</th>
-    <th>TempatLahir</th>
-    <th>TanggalLahir</th>
-    <th>Agama</th>
-    <th>Jenis Kelamin</th>
-    <th>NoHP</th>
-    <th>Opsi</th>
-  </tr>
-  @foreach($pegawai as $p)
-  <tr>
-    <td>{{$p->id}}</td>
-    <td>{{$p->NIP}}</td>
-    <td>{{$p->Nama}}</td>
-    <td>{{$p->Alamat}}</td>
-    <td>{{$p->TempatLahir}}</td>
-    <td>{{$p->TanggalLahir}}</td>
-    <td>{{$p->Agama}}</td>
-    <td>{{$p->JenisKelamin}}</td>
-    <td>{{$p->NoHP}}</td>
-    <td>
-      <a href="/pegawai/edit/{{ $p->id }}">Edit</a>
-      |
-      <a href="/pegawai/hapus/{{ $p->id }}">Hapus</a>
-    </td>
-  </tr>
-  @endforeach
-</table>
+  <table border="1">
+    <tr>
+      <th>id</th>
+      <th>NIP</th>
+      <th>Nama</th>
+      <th>Alamat</th>
+      <th>TempatLahir</th>
+      <th>TanggalLahir</th>
+      <th>Agama</th>
+      <th>Jenis Kelamin</th>
+      <th>NoHP</th>
+      <th>Opsi</th>
+    </tr>
+    @foreach($pegawai as $p)
+    <tr>
+      <td>{{$p->id}}</td>
+      <td>{{$p->NIP}}</td>
+      <td>{{$p->Nama}}</td>
+      <td>{{$p->Alamat}}</td>
+      <td>{{$p->TempatLahir}}</td>
+      <td>{{$p->TanggalLahir}}</td>
+      <td>{{$p->Agama}}</td>
+      <td>{{$p->JenisKelamin}}</td>
+      <td>{{$p->NoHP}}</td>
+      <td>
+        <a href="/pegawai/edit/{{ $p->id }}">Edit</a>
+        |
+        <a href="/pegawai/hapus/{{ $p->id }}">Hapus</a>
+      </td>
+    </tr>
+    @endforeach
+  </table>
+</body>
+
+</html>
 
 @endsection
