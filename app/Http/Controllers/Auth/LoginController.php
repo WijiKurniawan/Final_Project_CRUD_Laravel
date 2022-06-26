0a63afaeb939731627c9f48aa37208be701ed2f8
WijiKurniawan <wijikurniawan121@gmail.com>
2022-06-26 16:00:23 +0700

    redirect ke web pegawai

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,7 +26,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = '/pegawai';
 
     /**
      * Create a new controller instance.
