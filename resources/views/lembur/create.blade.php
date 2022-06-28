056a2b75e798ac229b3295d0aa9c1cdc11c37ef3
WijiKurniawan <wijikurniawan121@gmail.com>
2022-06-29 01:47:20 +0700

    create lembur

--- resources/views/lembur/create.blade.php
+++ resources/views/lembur/create.blade.php
@@ -18,11 +18,11 @@
             <input type="date" name="Tanggal" class="form-control" id="createPegawai" aria-describedby="">
         </div>
         <div class="mb-3">
-            <label for="createPegawai" class="form-label">Jam Lembur</label>
+            <label for="createPegawai" class="form-label">Lama Jam Lembur</label>
             <input type="text" name="JamLembur" class="form-control" id="createPegawai" aria-describedby="">
-        
 
-        <input type="submit" name="submit" class="btn btn-primary" value="Simpan">
+
+            <input type="submit" name="submit" class="btn btn-primary" value="Simpan">
     </form>
 </div>
 
