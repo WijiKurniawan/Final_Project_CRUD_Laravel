cde6e50a641411865fd253b17c527e6d59b3dd27
WijiKurniawan <wijikurniawan121@gmail.com>
2022-06-29 14:50:30 +0700

    perbaikan home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,50 +7,49 @@
     <button class="navbar-toggler " type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
       <span class="navbar-toggler-icon"></span>
     </button>
-    
+
     <a class="navbar-brand" href="#">DASHBOARD KEPEGAWAIAN</a>
 
     <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
+      <!-- Authentication Links -->
+      @guest
+      @if (Route::has('login'))
+      <li class="nav-item">
+        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+      </li>
+      @endif
 
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
+      @if (Route::has('register'))
+      <li class="nav-item">
+        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+      </li>
+      @endif
+      @else
+      <li class="nav-item dropdown">
+        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+          {{ Auth::user()->name }}
+        </a>
 
-                                
 
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
+
+        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+            {{ __('Logout') }}
+          </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-          <li class="nav-item">
+          <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+            @csrf
+          </form>
+        </div>
+      </li>
+      @endguest
+    </ul>
+    <li class="nav-item">
 
-          </li>
-        </ul>
-    
+    </li>
+    </ul>
+
     <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
       <div class="offcanvas-header">
         <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Kepegawaian</h5>
@@ -66,11 +65,11 @@
               Menu
             </a>
             <ul class="dropdown-menu text-black" aria-labelledby="offcanvasNavbarDropdown">
-            <li><a class="dropdown-item" href="{{url('pegawai')}}">Pegawai</a></li>
+              <li><a class="dropdown-item" href="{{url('pegawai')}}">Pegawai</a></li>
               <li>
                 <hr class="dropdown-divider">
               </li>
-            <li><a class="dropdown-item" href="{{url('absen')}}">Absen</a></li>
+              <li><a class="dropdown-item" href="{{url('absen')}}">Absen</a></li>
               <li>
                 <hr class="dropdown-divider">
               </li>
@@ -91,12 +90,12 @@
                 <hr class="dropdown-divider">
               </li>
               <li><a class="dropdown-item" href="{{url('pengalamankerja')}}">Pengalaman Kerja</a></li>
-                 
-             
+
+
             </ul>
           </li>
         </ul>
-        
+
       </div>
     </div>
   </div>
@@ -105,33 +104,33 @@
 <br>
 <br>
 <div class="row container-fluid"">
-  <div class="col-sm-6">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Pegawai</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content</p>
-        <a href="{{url('pegawai')}}" class="btn btn-primary">Telusuri</a>
-      </div>
-    </div>
-  </div>
-  <div class="col-sm-6">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Absen</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="{{url('absen')}}" class="btn btn-primary">Telusuri</a>
-      </div>
+  <div class=" col-sm-6">
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">Pegawai</h5>
+      <p class="card-text">Di halaman ini user dapat menambah data pegawai</p>
+      <a href="{{url('pegawai')}}" class="btn btn-primary">Telusuri</a>
     </div>
   </div>
 </div>
+<div class="col-sm-6">
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">Absen</h5>
+      <p class="card-text">Di halaman ini user dapat menambah data kehadiran pegawai</p>
+      <a href="{{url('absen')}}" class="btn btn-primary">Telusuri</a>
+    </div>
+  </div>
+</div>
+</div>
 <br>
 <br>
-<div class="row container-fluid"">
-  <div class="col-sm-6">
+<div class="row container-fluid">
+  <div class=" col-sm-6">
     <div class="card">
       <div class="card-body">
         <h5 class="card-title">Pendidikan Pegawai</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content</p>
+        <p class="card-text">Di halaman ini user dapat menambah data pendidikan pegawai</p>
         <a href="{{url('pendidikan')}}" class="btn btn-primary">Halaman Pendidikan Pegawai</a>
       </div>
     </div>
@@ -140,7 +139,7 @@
     <div class="card">
       <div class="card-body">
         <h5 class="card-title">Lembur</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <p class="card-text">Di halaman ini user dapat menambah dapat menambah data lembur pegawai</p>
         <a href="{{url('lembur')}}" class="btn btn-primary">Halaman Lembur</a>
       </div>
     </div>
@@ -148,12 +147,12 @@
 </div>
 <br>
 <br>
-<div class="row container-fluid"">
-  <div class="col-sm-6">
+<div class="row container-fluid">
+  <div class=" col-sm-6">
     <div class="card">
       <div class="card-body">
         <h5 class="card-title">JABATAN</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content</p>
+        <p class="card-text">Di halaman ini user dapat menambah data jabatan pegawai</p>
         <a href="{{url('jabatan')}}" class="btn btn-primary">Halaman Riwayat Jabatan</a>
       </div>
     </div>
@@ -162,7 +161,7 @@
     <div class="card">
       <div class="card-body">
         <h5 class="card-title">RIWAYAT JABATAN</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <p class="card-text">Di halaman ini user dapat menambah data riwayat jabatan pegawai</p>
         <a href="{{url('riwayat_jabatan')}}" class="btn btn-primary">Telusuri</a>
       </div>
     </div>
@@ -171,23 +170,23 @@
 <br>
 <br>
 <div class="row container-fluid"">
-  <div class="col-sm-6">
-    <div class="card">
-      <div class="card-body">
-        <img src="" class="card-img-top" alt="...">
-          <h5 class="card-title">Pengalaman Kerja</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content</p>
-        <a href="{{url('pengalamankerja')}}" class="btn btn-primary">Lihat</a>
-      </div>
+  <div class=" col-sm-6">
+  <div class="card">
+    <div class="card-body">
+      <img src="" class="card-img-top" alt="...">
+      <h5 class="card-title">Pengalaman Kerja</h5>
+      <p class="card-text">Di halaman ini user dapat menambah data pengalaman kerja pegawai</p>
+      <a href="{{url('pengalamankerja')}}" class="btn btn-primary">Lihat</a>
     </div>
   </div>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  
+</div>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
 
 
 
